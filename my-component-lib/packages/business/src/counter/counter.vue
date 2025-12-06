3f357921ea6caa18f610b3212e67b5c6b545d906
bianwy <bianwy025@qq.com>
2025-12-06 12:51:52 +0800

    设计子项目里组件库的文件目录格式，并添加示例代码。

---
+++ my-component-lib/packages/business/src/counter/counter.vue
@@ -0,0 +1,11 @@
+<template>
+  <button @click="onClick">点击减1</button>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+const num = ref<number>(100);
+const onClick = () => {
+  num.value += 1;
+};
+</script>
---
+++ my-component-lib/packages/components/src/box/box.vue
@@ -0,0 +1,10 @@
+<template>
+  <div :class="[`${prefixName}-box`, className]">
+    <slot v-if="$slots.default"></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { prefixName } from '../theme/index';
+defineProps({className: String});
+</script>
