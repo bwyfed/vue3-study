20503677436425da979e71b82c86874525f56806
bianwy <bianwy025@qq.com>
2025-12-18 10:56:39 +0800

    实现对话框组件和相关实例功能。

---
+++ my-component-lib/packages/components/examples/dialog.vue
@@ -0,0 +1,16 @@
+<template>
+  <button @click="onClick">点击打开对话框</button>
+</template>
+
+<script setup lang="ts">
+import { Dialog } from "../src";
+
+const onClick = () => {
+  const dialog = Dialog.createDialog({
+    text: "您好,我是对话框组件",
+    onOk: () => {
+      dialog.close();
+    },
+  });
+};
+</script>
---
+++ my-component-lib/packages/components/src/dialog/dialog.tsx
@@ -0,0 +1,28 @@
+import { defineComponent } from "vue";
+import { prefixName } from "../theme/index";
+
+export const DialogComponent = defineComponent({
+  name: "Dialog",
+  props: {
+    text: String,
+  },
+  emits: ["onOk"],
+  setup(props, context) {
+    const { emit } = context;
+    const onOk = () => {
+      emit("onOk");
+    };
+    return () => (
+      <div class={`${prefixName}-dialog-mask`}>
+        <div class={`${prefixName}-dialog`}>
+          <div class={`${prefixName}-dialog-text`}>{props.text}</div>
+          <div class={`${prefixName}-dialog-footer`}>
+            <button class={`${prefixName}-dialog-btn`} onClick={onOk}>
+              确定
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  },
+});
