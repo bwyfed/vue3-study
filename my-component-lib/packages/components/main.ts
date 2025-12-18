cbb300b5e95753cafdf46eb1f417373a5f0bb31c
bianwy <bianwy025@qq.com>
2025-12-18 14:01:01 +0800

    实现布局组件及其示例。

--- my-component-lib/packages/components/main.ts
+++ my-component-lib/packages/components/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from "vue";
 // import Example from "./examples/message.vue";
 // import Example from "./examples/dialog.vue";
-import Example from "./examples/gird.vue";
+// import Example from "./examples/gird.vue";
+import Example from "./examples/layout.vue";
 import "./src/index.less";
 
 const app = createApp(Example);
