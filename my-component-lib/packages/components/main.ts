86b5bd2e9c5c538980ea5de5ec8c7c0dd0401e0b
bianwy <bianwy025@qq.com>
2025-12-18 13:15:57 +0800

    实现栅格组件及其示例。

--- my-component-lib/packages/components/main.ts
+++ my-component-lib/packages/components/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 // import Example from "./examples/message.vue";
-import Example from "./examples/dialog.vue";
+// import Example from "./examples/dialog.vue";
+import Example from "./examples/gird.vue";
 import "./src/index.less";
 
 const app = createApp(Example);
